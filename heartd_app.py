bd469035c1536d6749fd55b4ef489e0c82b628e5
xuanji98 <xuanjiw98@gmail.com>
2024-07-13 11:06:44 +0700

    Update heartd_app.py

--- heartd_app.py
+++ heartd_app.py
@@ -25,7 +25,7 @@ scaler = MinMaxScaler()
 X_smote = scaler.fit_transform(X_smote)
 
 # Memuat model
-model = pickle.load(open('Model/rf_model_normalisasi.pkl', 'rb'))
+model = pickle.load(open('Model/knn_model_normalisasi.pkl', 'rb'))
 
 # Model Evaluation
 y_pred = model.predict(X_smote)
